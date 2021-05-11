bf8ec7256a2b69ff532ce46ac8cf49dc0903eb70
maazorawala <maazmaqbool995@gmail.com>
2021-05-12 01:05:53 +0530

    some changes
    some changes

--- testingApp/App.js
+++ testingApp/App.js
@@ -7,7 +7,6 @@
  */
 
 import React from 'react';
-import type {Node} from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -16,8 +15,13 @@ import {
   Text,
   useColorScheme,
   View,
+  FlatView,
+  FlatList,
+  TouchableOpacity,
+  Image,
+  Button,
 } from 'react-native';
-
+import Carousel, { Pagination } from       'react-native-x-carousel';
 import {
   Colors,
   DebugInstructions,
@@ -26,64 +30,66 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
+const DATA =[
+  {
+    text:'#1' ,
+    converimg:'https://i.ibb.co/Ry0ndhC/f1collage-1.jpg' 
+  },
+  {
+    text:'#2',
+    converimg:'https://i.ibb.co/GJLKQRr/jos.jpg',
+  },
+  {
+    text:'#3',
+    converimg:'https://i.ibb.co/sJSxb7N/max.jpg'
+  },
+];
+const App= () => {
+  const getFullName = (firstName, secondName, thirdName) =>{
+    return firstName + " " + secondName + " " + thirdName;
+  }
+  const _onPress =()=>{
+    alert('You have tapped the button')
+  }
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
+  const isDarkMode = useColorScheme() === 'dark';
 
+  const renderItem = data =>(
+    <View key={data.text} style={styles.item} >
+      <Image source={{uri: data.converimg}} style={{width:350, height:200, overflow:'hidden', margin:20, borderRadius:20}} />
+    </View>
+  )
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+     <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'light-content'} />
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+        <View style={{backgroundColor: isDarkMode ? Colors.black : Colors.white,color: isDarkMode ? Colors.light : Colors.dark}}>
+
+          <Text style={[{color: isDarkMode ? Colors.white : Colors.black}, styles.designText]} >Sample text</Text>
+
+          <Text style={[{color: isDarkMode ? Colors.white : Colors.black}]}>Testing Sned it Boiiiiiccv</Text>
+
+          <Text style={[{color: isDarkMode ? Colors.white : Colors.black}]}>Hello I am {getFullName("Maaz", "Maqbool", "Orawala")} </Text>
+
+      <Button onPress={_onPress} title="Press me" style={styles.buttonNew} />
+
+      <View style={styles.containerCaar}>
+
+        <Carousel   pagination={Pagination} renderItem={renderItem} data={DATA} autoplay loop />
+
+      </View>
+      <View style={[styles.container, {
+      flexDirection: "column"
+    }]}>
+      <View style={[{backgroundColor: isDarkMode ? Colors.black : Colors.black,}, {flex:1}]} />
+      <View style={{ flex: 2, backgroundColor: "darkorange" }} />
+      <View style={{ flex: 3, backgroundColor: "green" }} />
+    </View>
+      
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -91,21 +97,31 @@ const App: () => Node = () => {
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  designText:{
+    fontSize:40,
+    
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  container: {
+    flex: 1,
+    height:500,
+    width:'100%',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  buttonNew:{
+    margin:20,
+    backgroundColor:'#fff',
   },
-  highlight: {
-    fontWeight: '700',
+  containerCaar: {
+    flex: 1,
+    backgroundColor: '#ddd',
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin:10,
+    borderRadius:20,
+  },
+  item: {
+    
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
 
